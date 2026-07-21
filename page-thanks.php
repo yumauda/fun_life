6fecbad99139ff51bae9d3a807f55d8831c08237
yumauda <firststep-yumauda@gmail.com>
2026-07-21 21:53:10 +0900

    サンクスページを追加

--- page-thanks.php
+++ page-thanks.php
@@ -1,15 +1,21 @@
 <?php get_header(); ?>
 
 <main>
-  <section class="p-contact p-contact--thanks">
-    <div class="p-contact__inner l-inner">
-      <header class="p-contact__heading">
-        <h1 class="p-contact__title">CONTACT</h1>
-        <p class="p-contact__subtitle">お問い合わせ完了</p>
-        <p class="p-contact__intro">お問い合わせいただき、ありがとうございます。<br>内容を確認のうえ、担当者よりご連絡いたします。</p>
-      </header>
-      <div class="p-contact__thanks-link">
-        <a href="<?php echo esc_url(home_url('/')); ?>">トップページへ戻る</a>
+  <section class="p-thanks">
+    <div class="l-inner">
+      <div class="p-thanks__content">
+        <header class="p-thanks__heading">
+          <h1 class="p-thanks__title">THANK<br>YOU</h1>
+          <p class="p-thanks__subtitle">お問い合わせが完了しました</p>
+        </header>
+
+        <div class="p-thanks__message">
+          <p class="p-thanks__lead">この度はお問い合わせいただき、<br>誠にありがとうございます。</p>
+          <p class="p-thanks__text">ご入力いただいたメールアドレス宛に、自動返信メールをお送りしました。<br>お問い合わせ内容を確認のうえ、担当者より改めてご連絡いたします。</p>
+          <p class="p-thanks__notice">※しばらく経ってもメールが届かない場合は、入力されたメールアドレスが間違っているか、迷惑メールフォルダに振り分けられている可能性がございます。お手数ですがメールの受信設定をご確認のうえ、再度お問い合わせください。</p>
+        </div>
+
+        <a class="p-thanks__button" href="<?php echo esc_url(home_url('/')); ?>">TOPにもどる</a>
       </div>
     </div>
   </section>
